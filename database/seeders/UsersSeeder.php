52e07a1173e9675d106a83efff64e513fe3152d6
Filipe DEV <filipedomingos198@gmail.com>
2026-06-25 14:46:43 +0200

    Adiciona Seeder de Admin

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $admin = User::firstOrCreate([
+            'email' => 'admin@unilicungo.ac.mz',
+        ], [
+            'name' => 'Admin TechHub',
+            'password' => Hash::make('admin123'),
+            'email_verified_at' => now(),
+        ]);
+
+        $this->command->info('Admin criado/atualizado: ' . $admin->email);
+    }
+}
